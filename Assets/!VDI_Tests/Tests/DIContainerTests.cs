137a350c2967e993ab2ca63b36b3e742e7063b99
VladB <vladislav.b@bimiboo.com>
2024-11-13 18:55:00 +0300

    Create assembly for tests

---
+++ Assets/!VDI_Tests/Tests/DIContainerTests.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace VDI_Tests
+{
+    public class DIContainerTests
+    {
+        [Test]
+        public void DIContainerTestsSimplePasses()
+        {
+        }
+    }
+}
